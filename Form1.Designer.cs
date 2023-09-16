f9afdc9aa1209838fb15c06e5dc39027b53e7b2b
DarrenRobertLowe <61787379+DarrenRobertLowe@users.noreply.github.com>
2023-09-16 18:59:57 +0100

    Side panel now uses thumbnails instead of text
    Note: Selecting a folder with lots of large images will slow things down due to thumbnail generation. Might have to find an async solution.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,22 +29,25 @@ namespace PictureViewer
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.closeButton = new System.Windows.Forms.Button();
-            this.backgroundButton = new System.Windows.Forms.Button();
-            this.clearButton = new System.Windows.Forms.Button();
             this.showButton = new System.Windows.Forms.Button();
             this.nextImageButton = new System.Windows.Forms.Button();
             this.previousImageButton = new System.Windows.Forms.Button();
+            this.fileInfoText1 = new System.Windows.Forms.RichTextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.ToggleThumbnailsButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -56,8 +59,8 @@ namespace PictureViewer
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -65,12 +68,14 @@ namespace PictureViewer
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1045, 626);
             this.tableLayoutPanel1.TabIndex = 0;
             this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(159, 3);
@@ -79,15 +84,15 @@ namespace PictureViewer
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.closeButton);
-            this.flowLayoutPanel1.Controls.Add(this.backgroundButton);
-            this.flowLayoutPanel1.Controls.Add(this.clearButton);
             this.flowLayoutPanel1.Controls.Add(this.showButton);
             this.flowLayoutPanel1.Controls.Add(this.nextImageButton);
             this.flowLayoutPanel1.Controls.Add(this.previousImageButton);
+            this.flowLayoutPanel1.Controls.Add(this.fileInfoText1);
+            this.flowLayoutPanel1.Controls.Add(this.checkBox1);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(159, 566);
@@ -95,53 +100,20 @@ namespace PictureViewer
             this.flowLayoutPanel1.Size = new System.Drawing.Size(883, 57);
             this.flowLayoutPanel1.TabIndex = 5;
             // 
-            // closeButton
-            // 
-            this.closeButton.AutoSize = true;
-            this.closeButton.Location = new System.Drawing.Point(805, 3);
-            this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(75, 23);
-            this.closeButton.TabIndex = 3;
-            this.closeButton.Text = "Close";
-            this.closeButton.UseVisualStyleBackColor = true;
-            this.closeButton.Click += new System.EventHandler(this.closeProgram_Click);
-            // 
-            // backgroundButton
-            // 
-            this.backgroundButton.AutoSize = true;
-            this.backgroundButton.Location = new System.Drawing.Point(662, 3);
-            this.backgroundButton.Name = "backgroundButton";
-            this.backgroundButton.Size = new System.Drawing.Size(137, 23);
-            this.backgroundButton.TabIndex = 2;
-            this.backgroundButton.Text = "Set the background color";
-            this.backgroundButton.UseVisualStyleBackColor = true;
-            this.backgroundButton.Click += new System.EventHandler(this.setBackground_Click);
-            // 
-            // clearButton
-            // 
-            this.clearButton.AutoSize = true;
-            this.clearButton.Location = new System.Drawing.Point(562, 3);
-            this.clearButton.Name = "clearButton";
-            this.clearButton.Size = new System.Drawing.Size(94, 23);
-            this.clearButton.TabIndex = 1;
-            this.clearButton.Text = "Clear the picture";
-            this.clearButton.UseVisualStyleBackColor = true;
-            this.clearButton.Click += new System.EventHandler(this.clearImage_Click);
-            // 
             // showButton
             // 
             this.showButton.AutoSize = true;
-            this.showButton.Location = new System.Drawing.Point(468, 3);
+            this.showButton.Location = new System.Drawing.Point(792, 3);
             this.showButton.Name = "showButton";
             this.showButton.Size = new System.Drawing.Size(88, 23);
             this.showButton.TabIndex = 0;
-            this.showButton.Text = "Select picture";
+            this.showButton.Text = "Open";
             this.showButton.UseVisualStyleBackColor = true;
             this.showButton.Click += new System.EventHandler(this.openImage_Click);
             // 
             // nextImageButton
             // 
-            this.nextImageButton.Location = new System.Drawing.Point(387, 3);
+            this.nextImageButton.Location = new System.Drawing.Point(711, 3);
             this.nextImageButton.Name = "nextImageButton";
             this.nextImageButton.Size = new System.Drawing.Size(75, 23);
             this.nextImageButton.TabIndex = 7;
@@ -151,7 +123,7 @@ namespace PictureViewer
             // 
             // previousImageButton
             // 
-            this.previousImageButton.Location = new System.Drawing.Point(306, 3);
+            this.previousImageButton.Location = new System.Drawing.Point(630, 3);
             this.previousImageButton.Name = "previousImageButton";
             this.previousImageButton.Size = new System.Drawing.Size(75, 23);
             this.previousImageButton.TabIndex = 6;
@@ -159,18 +131,57 @@ namespace PictureViewer
             this.previousImageButton.UseVisualStyleBackColor = true;
             this.previousImageButton.Click += new System.EventHandler(this.previousImageButton_Click);
             // 
+            // fileInfoText1
+            // 
+            this.fileInfoText1.Location = new System.Drawing.Point(438, 3);
+            this.fileInfoText1.Name = "fileInfoText1";
+            this.fileInfoText1.Size = new System.Drawing.Size(186, 45);
+            this.fileInfoText1.TabIndex = 8;
+            this.fileInfoText1.Text = "";
+            // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.Checked = true;
             this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(3, 566);
+            this.checkBox1.Location = new System.Drawing.Point(320, 3);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(112, 17);
             this.checkBox1.TabIndex = 4;
             this.checkBox1.Text = "Image Fill Window";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(150, 557);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.ToggleThumbnailsButton);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 566);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(150, 57);
+            this.flowLayoutPanel2.TabIndex = 7;
+            // 
+            // ToggleThumbnailsButton
+            // 
+            this.ToggleThumbnailsButton.Location = new System.Drawing.Point(3, 3);
+            this.ToggleThumbnailsButton.Name = "ToggleThumbnailsButton";
+            this.ToggleThumbnailsButton.Size = new System.Drawing.Size(21, 23);
+            this.ToggleThumbnailsButton.TabIndex = 5;
+            this.ToggleThumbnailsButton.Text = "<";
+            this.ToggleThumbnailsButton.UseVisualStyleBackColor = true;
+            this.ToggleThumbnailsButton.Click += new System.EventHandler(this.ToggleThumbnailsButton_Click);
             // 
             // openFileDialog1
             // 
@@ -180,17 +191,11 @@ namespace PictureViewer
             this.openFileDialog1.Title = "Select a picture file";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
-            // listView1
+            // imageList1
             // 
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(3, 3);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(150, 557);
-            this.listView1.TabIndex = 6;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.SmallIcon;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
             // Form1
             // 
@@ -201,10 +206,10 @@ namespace PictureViewer
             this.Name = "Form1";
             this.Text = "Picture Viewer";
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -216,14 +221,15 @@ namespace PictureViewer
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button showButton;
-        private System.Windows.Forms.Button clearButton;
-        private System.Windows.Forms.Button backgroundButton;
-        private System.Windows.Forms.Button closeButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.Button previousImageButton;
         private System.Windows.Forms.Button nextImageButton;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.RichTextBox fileInfoText1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button ToggleThumbnailsButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,28 +13,39 @@ namespace PictureViewer
     public partial class Form1 : Form
     {
         string folder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-
         List<string> filteredFiles = new List<string>(); // used to store the files from the chosen folder
-
         private int iteration = 0;
-
-        
+        private int thumbnailWidth  = 128;
+        private int thumbnailHeight = 96;
+        private bool thumbnailsToggleState = true;
+        private int tableLayoutPanel1PreviousWidth;
 
 
         public Form1()
         {
             InitializeComponent();
+
+            //pictureBox1.WaitOnLoad = false;
+            imageList1 = new ImageList();
+            imageList1.ImageSize = new Size(thumbnailWidth, thumbnailHeight);
+
+            imageList1.ColorDepth = ColorDepth.Depth24Bit;
+            tableLayoutPanel1PreviousWidth = tableLayoutPanel1.Width;
+
             updateNextPrev();
             updateViewList();
         }
 
         private void updateViewList()
         {
+            listView1.Items.Clear();
             foreach (String s in filteredFiles)
             {
                 string filename = System.IO.Path.GetFileName(s);
                 listView1.Items.Add(filename);
             }
+
+            getThumbnails();
         }
 
 
@@ -42,16 +53,16 @@ namespace PictureViewer
         /// <summary>
         /// enables/disables the next and previous buttons depending on file count in folder
         /// </summary>
-        private void updateNextPrev() 
+        private void updateNextPrev()
         {
-            if (filteredFiles.Count <= 1) 
+            if (filteredFiles.Count <= 1)
             {
-                nextImageButton.Enabled     = false;
+                nextImageButton.Enabled = false;
                 previousImageButton.Enabled = false;
             }
-            else 
+            else
             {
-                nextImageButton.Enabled     = true;
+                nextImageButton.Enabled = true;
                 previousImageButton.Enabled = true;
             }
         }
@@ -61,25 +72,38 @@ namespace PictureViewer
             if (pictureBox1.SizeMode == PictureBoxSizeMode.Normal)
             {
                 pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            } 
+            }
             else
             {
                 pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
             }
-            
+
         }
 
-        private void openImage_Click(object sender, EventArgs e)  // select an image
+        private void openImage_Click(object sender, EventArgs e)  // open an image
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.Load(openFileDialog1.FileName);
                 folder = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
                 filteredFiles = getFiles();
+
+                updateFileInfo();
                 updateNextPrev();
                 updateViewList();
             }
         }
+
+        private void updateFileInfo()
+        {
+            fileInfoText1.Text = "File: " + pictureBox1.ImageLocation;
+            fileInfoText1.Text += "\n";
+            fileInfoText1.Text += "Size: " + pictureBox1.Image.Width + " x " + pictureBox1.Image.Height;
+            fileInfoText1.Text += "\n";
+            fileInfoText1.Text += "PPI: " + pictureBox1.Image.HorizontalResolution;// +" x " + pictureBox1.Image.VerticalResolution;
+            fileInfoText1.Text += "\n";
+        }
+
 
         private void clearImage_Click(object sender, EventArgs e) // clear the picture
         {
@@ -94,10 +118,13 @@ namespace PictureViewer
             }
         }
 
+        /*
         private void closeProgram_Click(object sender, EventArgs e) // close
         {
             this.Close();
         }
+        */
+
 
         /// <summary>
         /// Iterates backwards through the filteredFiles list
@@ -115,6 +142,7 @@ namespace PictureViewer
 
             String file = filteredFiles[iteration];
             pictureBox1.Load(file);
+            updateFileInfo();
         }
 
         /// <summary>
@@ -133,10 +161,10 @@ namespace PictureViewer
 
             String file = filteredFiles[iteration];
             pictureBox1.Load(file);
+            updateFileInfo();
         }
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
-
         }
 
         /// <summary>
@@ -158,9 +186,38 @@ namespace PictureViewer
             return allFilesList;
         }
 
+        private void getThumbnails()
+        {
+            imageList1.Images.Clear();
+
+            for (var i = 0; i < listView1.Items.Count; i++)
+            {
+                var imageUrl = (folder + "\\" + listView1.Items[i].Text);
+                var image = System.Drawing.Image.FromFile(imageUrl);
+
+                Bitmap img = new Bitmap(image);
+                Point p = new Point(0);
+                Rectangle area = new Rectangle(new Point(0), new Size(Math.Min(image.Width, thumbnailWidth), Math.Min(image.Height, thumbnailHeight)));
+                img.Clone(area, img.PixelFormat);
+
+                imageList1.Images.Add("" + i, img);
+            }
+
+            listView1.LargeImageList = imageList1;
+
+            for (var i = 0; i < listView1.Items.Count; i++)
+            {
+                var file = listView1.Items[i];
+                file.ImageIndex = i;
+            }
+
+
+        }
+
+
+
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
@@ -169,7 +226,45 @@ namespace PictureViewer
             {
                 string selected = listView1.SelectedItems[0].Text;
                 pictureBox1.Load(folder + "\\" + selected);
+                updateFileInfo();
             }
         }
+
+        private void ToggleThumbnailsButton_Click(object sender, EventArgs e)
+        {
+            
+            if (thumbnailsToggleState == true)
+            {
+
+                //tableLayoutPanel1PreviousWidth = tableLayoutPanel1.Width;
+                //tableLayoutPanel1.ColumnStyles[1].Width = 0;//Hide();
+                thumbnailsToggleState = false;
+                ToggleThumbnailsButton.Text = ">";
+                Console.WriteLine("Hidden");
+            }
+            else
+            {
+                //tableLayoutPanel1.ColumnStyles[1].Width = tableLayoutPanel1PreviousWidth;//Show();
+                thumbnailsToggleState = true;
+                ToggleThumbnailsButton.Text = "<";
+                Console.WriteLine("Visible");
+            }
+            
+        }
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Picture has been clicked. Now would be a good time to full screen it.");
+        }
+
+
+        /*
+        private void pictureBox1_LoadCompleted(Object sender, AsyncCompletedEventArgs e)
+        {
+            updateFileInfo(); 
+        }
+        */
     }
 }
